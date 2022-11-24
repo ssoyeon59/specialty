31070771de28a42233a2dfa0a298bc7549590f09
ssoyeon59 <nebula059@naver.com>
2022-11-24 23:24:56 +0900

    Feat: Add week03 memo

---
+++ week03/src/main/java/com/sparta/week03/domain/Timestamped.java
@@ -0,0 +1,19 @@
+package com.sparta.week03.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass // Entity가 자동으로 컬럼으로 인식한다
+@EntityListeners(AuditingEntityListener.class)//생성/변경 시간을 자동으로 업데이트
+public class Timestamped {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
